d91668673b917febdb16db9f42fcc3beb5cb4b75
Plutonist <widesense.plqws@gmail.com>
2017-05-07 15:51:09 +0800

    stage-i

--- main.go
+++ main.go
@@ -1,64 +1,65 @@
 package main
 
 import (
-	"dynosocks/socks"
-	"net"
-
 	"github.com/armon/go-socks5"
+	//kcp "github.com/xtaci/kcp-go"
+	"net"
+	"time"
+	"github.com/xtaci/kcp-go"
+	"io"
 )
 
 func main() {
-	conf := &socks5.Config{}
-	ss, _ := socks5.New(conf)
-	ss.ListenAndServe("unix", "/tmp/dynosocks_socks5.sock")
-}
-
-func mfasfdsaain() {
-	go func() {
-		laddr, _ := net.ResolveTCPAddr("tcp", ":0")
-		server, _ := socks.ListenSocks(":1087")
-		//print(err.Error())
-		go func() {
-			for {
-				conn, _ := server.Accept()
-				go func() {
-					conn.Ready()
-					println("accept connection to:", conn.Target.String())
-					tcpConn, err := net.DialTCP("tcp", laddr, conn.Target)
-					if err != nil {
-						conn.Close()
-						return
-					}
-
-					go func() {
-						buf := make([]byte, 4096)
-						for {
-							length, err := conn.Read(buf)
-							if length == 0 || err != nil {
-								tcpConn.Close()
-								conn.Close()
-								return
-							}
-							tcpConn.Write(buf[:length])
-						}
-					}()
-
-					go func() {
-						buf := make([]byte, 4096)
-						for {
-							length, err := tcpConn.Read(buf)
-							if length == 0 || err != nil {
-								tcpConn.Close()
-								conn.Close()
-								return
-							}
-							conn.Write(buf[:length])
-						}
-					}()
-				}()
-			}
-		}()
-	}()
+	go server()
+	time.Sleep(1000)
+	go cli()
 
 	<- make(chan bool)
+}
+
+func cli() {
+	laddr, _ := net.ResolveTCPAddr("tcp", ":1087")
+	tcpListener, _ := net.ListenTCP("tcp", laddr)
+
+	for {
+		tcpConn, _ := tcpListener.Accept()
+		conn, _ := kcp.DialWithOptions("127.0.0.1:9980", nil, 10, 3)
+		go pipe(conn, tcpConn)
+	}
+}
+
+func server() {
+	go func() {
+		conf := &socks5.Config{}
+		ss, _ := socks5.New(conf)
+		ss.ListenAndServe("tcp", "127.0.0.1:9527")
+	}()
+
+	listener, _ := kcp.ListenWithOptions(":9980", nil, 10, 3)
+	//listener, _ := net.ListenTCP("tcp", zzbl_a)
+	for {
+		conn, _ := listener.AcceptKCP()
+		saddr, _ := net.ResolveTCPAddr("tcp", "127.0.0.1:9527")
+		laddr, _ := net.ResolveTCPAddr("tcp", ":0")
+		socksCli, _ := net.DialTCP("tcp", laddr, saddr)
+
+		println("remote accept", conn.RemoteAddr().String())
+		go pipe(conn, socksCli)
+	}
+}
+
+func pipe(s1, s2 io.ReadWriteCloser) {
+	defer s1.Close()
+	defer s2.Close()
+
+	s1Close := make(chan struct{})
+	go func() { io.Copy(s1, s2); close(s1Close) }()
+
+	s2Close := make(chan struct{})
+	go func() { io.Copy(s2, s1); close(s2Close) }()
+
+	select {
+	case <-s1Close:
+	case <-s2Close:
+	}
 }
